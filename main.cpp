816c7cce6ea66b287533fcf24b85a62c60e35a2d
strn18 <strn18@korea.ac.kr>
2023-02-02 12:29:41 +0900

    solved BOJ 2482

--- main.cpp
+++ main.cpp
@@ -1,49 +1,27 @@
 #include <iostream>
-#include <stack>
 #define MAX 1000
+#define DEN 1000000003
 
 using namespace std;
 
+int dp[MAX+1][MAX+1] = {0}; // dp[i][j]: 0~i번 색 중에서 j개 고르기
+
 int main(){
-  int N, max_idx, max_len = 0;
-  int num[MAX], dp[MAX], prev_idx[MAX];
-  stack<int> st;
+  int N, K;
 
   scanf("%d", &N);
+  scanf("%d", &K);
 
-  for(int i=0; i<N; i++){
-    int max_dp = 0;
-    prev_idx[i] = -1;
-
-    scanf("%d", &num[i]);
-
-    for(int j=0; j<i; j++){
-      if(num[j] < num[i] && dp[j] > max_dp){
-        max_dp = dp[j];
-        prev_idx[i] = j;
-      }
-    }
-
-    dp[i] = max_dp+1;
-
-    if(dp[i] > max_len){
-      max_len = dp[i];
-      max_idx = i;
-    }
+  for(int i=0; i<=N; i++){
+    dp[i][0] = 1;
+    dp[i][1] = i;
   }
 
-  st.push(num[max_idx]);
-  while(prev_idx[max_idx] != -1){
-    st.push(num[prev_idx[max_idx]]);
-    max_idx = prev_idx[max_idx];
-  }
+  for(int i=2; i<=N; i++)
+    for(int j=2; (j<=K && j<=i); j++)
+      dp[i][j] = (dp[i-2][j-1] + dp[i-1][j]) % DEN; // i번 색을 고를 경우 + i번 색을 안 고를 경우
 
-  printf("%d\n", max_len);
-
-  while(!st.empty()){
-    printf("%d ", st.top());
-    st.pop();
-  }
+  printf("%d", (dp[N-3][K-1] + dp[N-1][K]) % DEN); // 0번 색을 고를 경우 + 0번 색을 안 고를 경우
 
   return 0;
 }
