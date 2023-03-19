a51d6c9dbe6917869d4f498ec0195777fad1e0fd
strn18 <strn18@korea.ac.kr>
2023-03-19 12:32:06 +0900

    solved BOJ 17070

--- main.cpp
+++ main.cpp
@@ -1,74 +1,59 @@
 #include <iostream>
-#include <string>
-#include <queue>
+#define MAX 16
 
 using namespace std;
 
-int ans = 0;
-int dr[4] = {-1, 1, 0, 0}, dc[4] = {0, 0, -1, 1};
-int seven_board[5][5] = {0};
-string board[5];
+int wall[MAX][MAX];
+int dp[3][MAX][MAX];
 
-void combination(int num, int doyeon, int princess);
-bool check(int r, int c);
+int DP(int i, int r, int c);
 
 int main(){
-  for(int i=0; i<5; i++)
-    cin >> board[i];
+  int N, ans = 0;
 
-  for(int i=0; i<19; i++)
-    combination(i, 0, 0);
+  scanf("%d", &N);
+
+  for(int r=0; r<N; r++){
+    for(int c=0; c<N; c++){
+      scanf("%d", &wall[r][c]);
+      for(int i=0; i<3; i++)
+        dp[i][r][c] = -1;
+    }
+  }
+
+  for(int i=0; i<3; i++){
+    for(int r=0; r<MAX; r++){
+      dp[i][r][0] = 0;
+      dp[i][r][1] = 0;
+    }
+  }
+  dp[2][0][1] = 1;
+
+  for(int i=0; i<2; i++)
+    for(int c=0; c<MAX; c++)
+      dp[i][0][c] = 0;
+
+  for(int i=0; i<3; i++)
+    ans += DP(i, N-1, N-1);
   
   printf("%d", ans);
 
   return 0;
 }
 
-void combination(int num, int doyeon, int princess){
-  int r = num/5, c = num%5;
+int DP(int i, int r, int c){
+  if(dp[i][r][c] != -1) return dp[i][r][c];
 
-  if(board[r][c] == 'Y') doyeon++;
-  if(doyeon > 3) return;
+  if(wall[r][c]) return (dp[i][r][c] = 0);
 
-  seven_board[r][c] = 1;
-
-  if(++princess == 7){
-    if(check(r, c))
-      ans++;
+  if(i==0)
+    dp[0][r][c] = DP(0, r-1, c) + DP(1, r-1, c);
+  else if(i==1){
+    if(wall[r-1][c] || wall[r][c-1]) dp[1][r][c] = 0;
+    else dp[1][r][c] = DP(0, r-1, c-1) + DP(1, r-1, c-1) + DP(2, r-1, c-1);
   }
-  else{
-    for(int i=num+1; i<25; i++)
-      combination(i, doyeon, princess);
-  }
+  else
+    dp[2][r][c] = DP(1, r, c-1) + DP(2, r, c-1);
 
-  seven_board[r][c] = 0;
-}
-
-bool check(int r, int c){
-  queue<pair<int, int>> Q;
-  bool visited[5][5] = {false};
-  int count = 0;
-
-  Q.push({r, c});
-  visited[r][c] = true;
-  count++;
-
-  while(!Q.empty()){
-    int cur_r = Q.front().first, cur_c = Q.front().second;
-    Q.pop();
-
-    for(int i=0; i<4; i++){
-      int next_r = cur_r+dr[i], next_c = cur_c+dc[i];
-      
-      if(0<=next_r && next_r<5 && 0<=next_c && next_c<5){
-        if(seven_board[next_r][next_c] && !visited[next_r][next_c]){
-          Q.push({next_r, next_c});
-          visited[next_r][next_c] = true;
-          count++;
-        }
-      }
-    }
-  }
-
-  return (count == 7 ? true : false);
+  return dp[i][r][c];
 }
