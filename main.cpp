8c6edd6bb499af536a301911316757d7f9a78d2a
strn18 <strn18@korea.ac.kr>
2023-03-20 23:17:28 +0900

    solved BOJ 1041

--- main.cpp
+++ main.cpp
@@ -1,59 +1,41 @@
 #include <iostream>
-#define MAX 16
+#define ll long long
 
 using namespace std;
 
-int wall[MAX][MAX];
-int dp[3][MAX][MAX];
-
-int DP(int i, int r, int c);
-
 int main(){
-  int N, ans = 0;
+  ll N, count = 0;
+  ll num[6], min_num[3];
 
   scanf("%d", &N);
 
-  for(int r=0; r<N; r++){
-    for(int c=0; c<N; c++){
-      scanf("%d", &wall[r][c]);
-      for(int i=0; i<3; i++)
-        dp[i][r][c] = -1;
-    }
+  min_num[0] = 51;
+  for(int i=0; i<6; i++){
+    scanf("%lld", &num[i]);
+    min_num[0] = min(min_num[0], num[i]);
   }
-
-  for(int i=0; i<3; i++){
-    for(int r=0; r<MAX; r++){
-      dp[i][r][0] = 0;
-      dp[i][r][1] = 0;
-    }
-  }
-  dp[2][0][1] = 1;
-
-  for(int i=0; i<2; i++)
-    for(int c=0; c<MAX; c++)
-      dp[i][0][c] = 0;
-
-  for(int i=0; i<3; i++)
-    ans += DP(i, N-1, N-1);
   
-  printf("%d", ans);
+  if(N==1){
+    ll max_num = 0;
+    for(int i=0; i<6; i++){
+      count += num[i];
+      max_num = max(max_num, num[i]);
+    }
+    count -= max_num;
+  }
+  else{
+    int a = min(num[0], num[5]);
+    int b = min(num[1], num[4]);
+    int c = min(num[2], num[3]);
+    min_num[1] = min(min(a+b, a+c), b+c);
+    min_num[2] = a+b+c;
+
+    count += (min_num[0]*(N-2)*(N-2)*5);
+    count += (min_num[1]*(N-2)*8 + min_num[0]*(N-2)*4);
+    count += (min_num[2]*4 + min_num[1]*4);
+  }
+
+  printf("%lld", count);
 
   return 0;
-}
-
-int DP(int i, int r, int c){
-  if(dp[i][r][c] != -1) return dp[i][r][c];
-
-  if(wall[r][c]) return (dp[i][r][c] = 0);
-
-  if(i==0)
-    dp[0][r][c] = DP(0, r-1, c) + DP(1, r-1, c);
-  else if(i==1){
-    if(wall[r-1][c] || wall[r][c-1]) dp[1][r][c] = 0;
-    else dp[1][r][c] = DP(0, r-1, c-1) + DP(1, r-1, c-1) + DP(2, r-1, c-1);
-  }
-  else
-    dp[2][r][c] = DP(1, r, c-1) + DP(2, r, c-1);
-
-  return dp[i][r][c];
 }
