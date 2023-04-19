75c95d4503b8799184db38346b28e90047ae9bf3
strn18 <strn18@korea.ac.kr>
2023-04-19 21:29:24 +0900

    solved BOJ 2440, 1996

---
+++ algorithm/ezez/2440.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  int N;
+
+  cin >> N;
+
+  for(int i=N; i>=1; i--){
+    for(int j=0; j<i; j++)
+      cout << '*';
+    cout << '\n';
+  }
+
+  return 0;
+}
